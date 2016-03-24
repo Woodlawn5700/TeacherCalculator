59578490d2be3f845102571aa146420f02e7c5b3
pavelpetrov <Pavel124578@mail.ru>
2016-03-24 15:39:02 +0300

    add multiplications

--- src/ru/levelp/CalculatorModel.java
+++ src/ru/levelp/CalculatorModel.java
@@ -37,68 +37,87 @@ public class CalculatorModel {
         operationStack.push(operation);
         char sqr1 = 8730;
         String sqr2 = String.valueOf(sqr1);
+        double myResul = 0;
         if (operation.equals("=")) {
-                 operationStack.pop();
-                view.setResult(toDo());
+            operationStack.pop();
+            myResul = toDo();
 
         } else if (operationStack.peek().equals("sin")) {
-            view.setResult(Math.sin(Math.toRadians(operands.pop())));
+            myResul = Math.sin(Math.toRadians(operands.pop()));
         } else if (operation.equals("tan")) {
-            view.setResult(Math.tan(Math.toRadians(operands.pop())));
+            myResul = Math.tan(Math.toRadians(operands.pop()));
         } else if (operation.equals("atan")) {
-            view.setResult(Math.atan(Math.toRadians(operands.pop())));
+            myResul = Math.atan(Math.toRadians(operands.pop()));
         } else if (operation.equals("cos")) {
-            view.setResult(Math.cos(Math.toRadians(operands.pop())));
+            myResul = Math.cos(Math.toRadians(operands.pop()));
         } else if (operation.equals("ln")) {
-            view.setResult(Math.log(operands.pop()));
+            myResul = Math.log(operands.pop());
         } else if (operation.equals("lg")) {
-            view.setResult(Math.log10(operands.pop()));
+            myResul = Math.log10(operands.pop());
         } else if (operation.equals("log2")) {
-            view.setResult(Math.log10(operands.pop()) / Math.log10(2));
+            myResul = Math.log10(operands.pop()) / Math.log10(2);
         } else if (operation.equals("x^2")) {
             view.setResult(Math.pow(operands.pop(), 2));
         } else if (operation.equals(sqr2)) {
             view.setResult(Math.sqrt(operands.pop()));
-        }
-        else if (operation.equals("M+")){
-          memory = memory + operands.pop();
-            view.setResult(0);
-        }
-        else if (operation.equals("M-")){
-           memory -= operands.pop();
-            view.setResult(0);
-        }
-        else if (operation.equals("MC")){
-           memory = 0;
-        }
-        else if (operation.equals("MR")){
-            view.setResult(memory);
-        }
-        else if (operation.equals("^")) {
+        } else if (operation.equals("M+")) {
+            memory = memory + operands.pop();
+            myResul = 0;
+        } else if (operation.equals("M-")) {
+            memory -= operands.pop();
+            myResul = 0;
+        } else if (operation.equals("MC")) {
+            memory = 0;
+        } else if (operation.equals("MR")) {
+            myResul = memory;
+        } else if (operation.equals("^")) {
             double a = operands.pop();
             double b = operands.pop();
-            view.setResult(Math.pow(b, a));
-        }
-        else{
+            myResul = Math.pow(b, a);
+        } else {
             this.operation = operation;
-            view.setResult(0);
+            myResul = 0;
         }
+        view.setResult(myResul);
     }
 
     public double toDo() {
         double result = 0;
-            while (!operationStack.isEmpty())
+        while (!operationStack.isEmpty())
             if (operationStack.peek().equals("-")) {
                 double b = operands.pop();
+
                 double a = operands.pop();
-                result = a - b;
-                operands.push(result);
                 operationStack.pop();
 
-            } else if (operationStack.peek().equals("+")) {
-                result = operands.pop() + operands.pop();
+                if (!operationStack.isEmpty()) {
+                    if (operationStack.peek().equals("-")) {
+                        result = a + b;
+                    } else {
+                        result = a - b;
+                    }
+                } else {
+                    result = a - b;
+                }
                 operands.push(result);
+
+
+            } else if (operationStack.peek().equals("+")) {
+                double b = operands.pop();
+                double a = operands.pop();
                 operationStack.pop();
+                if (!operationStack.isEmpty()) {
+                    if (operationStack.peek().equals("-")) {
+                        result = a - b;
+                    } else {
+                        result = a + b;
+                    }
+                } else {
+                    result = a + b;
+                }
+
+                operands.push(result);
+
 
             } else if (operationStack.peek().equals("*")) {
                 result = operands.pop() * operands.pop();
@@ -113,8 +132,8 @@ public class CalculatorModel {
                 operationStack.pop();
             }
 
-            return result;
-        }
+        return result;
+    }
 
 
     /*
